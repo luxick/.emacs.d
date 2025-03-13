01e666537c03aede24e45b4977b8fee93af3d771
luxick <git@luxick.de>
2025-03-13 15:12:59 +0100

    Disable copilot

--- init.el
+++ init.el
@@ -162,6 +162,7 @@
 
 ;; Keybindings
 (bind-key "C-x k"      'kill-buffer-with-prejudice)
+(bind-key "C-w"        'kill-buffer-with-prejudice)
 (bind-key "C-x C-k"    'kill-buffer-and-window)
 (bind-key "M-i"        'delete-indentation)
 (bind-key "C-+"        'text-scale-increase)
@@ -338,13 +339,13 @@
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
 ;; Copilot
-(use-package copilot
-  :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
-  :ensure t)
-(add-hook 'prog-mode-hook 'copilot-mode)
-(define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
-(define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
-(define-key copilot-completion-map (kbd "M--") 'copilot-complete)
+;; (use-package copilot
+;;   :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
+;;   :ensure t)
+;; (add-hook 'prog-mode-hook 'copilot-mode)
+;; (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
+;; (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
+;; (define-key copilot-completion-map (kbd "C-.") 'copilot-complete)
 
 ;; Elisp
 (defun my-elisp-mode-hook ()

50c9660cf5d0b29042493dd2a28acaa0505324fe
luxick <git@luxick.de>
2025-05-03 14:23:30 +0200

    Lower default font size

--- init.el
+++ init.el
@@ -60,7 +60,7 @@
 (tooltip-mode -1)
 (fringe-mode -1)
 ;; Font settings
-(set-face-attribute 'default nil :font "Inconsolata-12")
+(set-face-attribute 'default nil :font "Inconsolata-10")
 (set-face-attribute 'fixed-pitch nil :family 'unspecified)
 (set-face-attribute 'variable-pitch nil :family 'unspecified)
 

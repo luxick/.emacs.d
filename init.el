477484f047305cb1aa6b6a38efb481818120c0ea
luxick <git@luxick.de>
2025-05-22 09:50:19 +0200

    Remove paredit-mode package

--- init.el
+++ init.el
@@ -337,9 +337,6 @@
 (setq org-startup-folded t)
 
 ;; Programming configuration
-(use-package paredit
-  :straight t)
-(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
 (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
 (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
